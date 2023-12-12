ff85761cf0b1c882e88bb4c330509567ccfe4c7e
Ayronman99 <itechman32@gmail.com>
2023-12-12 11:36:24 +0800

    initial commit!!!

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import '@mantine/core/styles.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+import { theme } from '../theme';
+import { Header } from '@/layout/HeaderSimple';
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider theme={theme}>
+      <Head>
+        <title>Salary Computer</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <Header />
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
+}
